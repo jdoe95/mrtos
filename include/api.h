e02606459a42eb7ba4f7cfc79080e9a6fbe17696
John Buyi Yu <buyi.yu@wne.edu>
2018-11-07 15:17:25 -0500

    fixup! prettified api.h

--- include/api.h
+++ include/api.h
@@ -35,11 +35,11 @@ typedef struct {
 	os_uint_t pool_size; /* pool size, must be aligned              */
 } os_config_t;
 
-void      os_init( const os_config_t *p_config );
-void      os_start( void );
+void      os_init                 ( const os_config_t *p_config );
+void      os_start                ( void );
 os_uint_t os_get_heartbeat_counter( void );
-void      os_enter_critical( void );
-void      os_exit_critical( void );
+void      os_enter_critical       ( void );
+void      os_exit_critical        ( void );
 
 /* Information about a memory block */
 typedef struct {
@@ -58,11 +58,11 @@ typedef struct {
 	os_uint_t num_blocks;  /* number of memory blocks allocated to thread */
 } os_memory_thread_info_t;
 
-void * os_memory_allocate( os_uint_t size );
-void   os_memory_free( void *p );
-void   os_memory_get_block_info( void *p, os_memory_block_info_t *p_info );
+void * os_memory_allocate       ( os_uint_t size );
+void   os_memory_free           ( void *p );
+void   os_memory_get_block_info ( void *p, os_memory_block_info_t *p_info );
 void   os_memory_get_thread_info( os_handle_t h_thread, os_memory_thread_info_t *p_info );
-void   os_memory_get_pool_info( os_memory_pool_info_t *p_info );
+void   os_memory_get_pool_info  ( os_memory_pool_info_t *p_info );
 
 typedef enum
 {
@@ -72,36 +72,36 @@ typedef enum
 	OS_THREAD_DELETED
 } os_thread_state_t;
 
-os_handle_t       os_thread_create( os_uint_t prio, os_uint_t stack_size, void (*p_job)(void) );
-void              os_thread_delete( os_handle_t h_thread );
-void              os_thread_suspend( os_handle_t h_thread );
-void              os_thread_resume( os_handle_t h_thread );
-os_thread_state_t os_thread_get_state( os_handle_t h_thread );
-os_handle_t       os_thread_get_current( void );
+os_handle_t       os_thread_create      ( os_uint_t prio, os_uint_t stack_size, void(*p_job) (void) );
+void              os_thread_delete      ( os_handle_t h_thread );
+void              os_thread_suspend     ( os_handle_t h_thread );
+void              os_thread_resume      ( os_handle_t h_thread );
+os_thread_state_t os_thread_get_state   ( os_handle_t h_thread );
+os_handle_t       os_thread_get_current ( void );
 void              os_thread_set_priority( os_handle_t h_thread, os_uint_t prio );
 os_uint_t         os_thread_get_priority( os_handle_t h_thread );
-void              os_thread_yield( void );
-void              os_thread_delay( os_uint_t timeout );
+void              os_thread_yield       ( void );
+void              os_thread_delay       ( os_uint_t timeout );
 
-os_handle_t os_semaphore_create( os_uint_t initial);
-void        os_semaphore_delete( os_handle_t h_sem );
-void        os_semaphore_reset( os_handle_t h_sem, os_uint_t initial );
-os_uint_t   os_semaphore_get_counter( os_handle_t h_sem );
-void        os_semaphore_post( os_handle_t h_sem );
-os_bool_t   os_semaphore_wait( os_handle_t h_sem, os_uint_t timeout );
+os_handle_t os_semaphore_create          ( os_uint_t initial);
+void        os_semaphore_delete          ( os_handle_t h_sem );
+void        os_semaphore_reset           ( os_handle_t h_sem, os_uint_t initial );
+os_uint_t   os_semaphore_get_counter     ( os_handle_t h_sem );
+void        os_semaphore_post            ( os_handle_t h_sem );
+os_bool_t   os_semaphore_wait            ( os_handle_t h_sem, os_uint_t timeout );
 os_bool_t   os_semaphore_wait_nonblocking( os_handle_t h_sem );
-os_bool_t   os_semaphore_peek_wait( os_handle_t h_sem );
+os_bool_t   os_semaphore_peek_wait       ( os_handle_t h_sem );
 
-os_handle_t os_mutex_create( void );
-void        os_mutex_delete( os_handle_t h_mutex );
-os_bool_t   os_mutex_peek_lock( os_handle_t h_mutex );
-os_bool_t   os_mutex_peek_lock_recursive( os_handle_t h_mutex);
-os_bool_t   os_mutex_is_locked( os_handle_t h_mutex );
-os_bool_t   os_mutex_lock_nonblocking( os_handle_t h_mutex );
+os_handle_t os_mutex_create                    ( void );
+void        os_mutex_delete                    ( os_handle_t h_mutex );
+os_bool_t   os_mutex_peek_lock                 ( os_handle_t h_mutex );
+os_bool_t   os_mutex_peek_lock_recursive       ( os_handle_t h_mutex);
+os_bool_t   os_mutex_is_locked                 ( os_handle_t h_mutex );
+os_bool_t   os_mutex_lock_nonblocking          ( os_handle_t h_mutex );
 os_bool_t   os_mutex_lock_recursive_nonblocking( os_handle_t h_mutex );
-os_bool_t   os_mutex_lock( os_handle_t h_mutex, os_uint_t timeout );
-os_bool_t   os_mutex_lock_recursive( os_handle_t h_mutex, os_uint_t timeout );
-void        os_mutex_unlock( os_handle_t h_mutex );
+os_bool_t   os_mutex_lock                      ( os_handle_t h_mutex, os_uint_t timeout );
+os_bool_t   os_mutex_lock_recursive            ( os_handle_t h_mutex, os_uint_t timeout );
+void        os_mutex_unlock                    ( os_handle_t h_mutex );
 
 
 #endif /* H1CB9096F_13C2_4118_B608_F147C53BE57D */
