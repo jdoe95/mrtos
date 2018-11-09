2715f0e9fce8f0d1e999c1afe4c4d3dc2a1e0784
John Buyi Yu <buyi.yu@wne.edu>
2018-11-09 15:37:34 -0500

    added module include file

---
+++ rtos_module.h
@@ -0,0 +1,41 @@
+/** ************************************************************************
+ * @file rtos_module.h
+ * @brief External include header
+ * @author John Yu buyi.yu@wne.edu
+ *
+ * This file is part of mRTOS.
+ *
+ * Copyright (C) 2018 John Buyi Yu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *****************************************************************************/
+#ifndef H10443F26_8333_43E2_ACCD_FC9E34241DE7
+#define H10443F26_8333_43E2_ACCD_FC9E34241DE7
+
+#include "include/portable.h"
+#include "include/util.h"
+#include "include/list.h"
+#include "include/memory.h"
+#include "include/global.h"
+#include "include/thread.h"
+#include "include/semaphore.h"
+#include "include/mutex.h"
+#include "include/api.h"
+
+#endif /* H10443F26_8333_43E2_ACCD_FC9E34241DE7 */
